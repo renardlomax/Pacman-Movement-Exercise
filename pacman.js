ca1c44069955782cfeba8e57a7d8f8e88b369e85
Renard Lomax <76239901+renardlomax@users.noreply.github.com>
2021-03-23 11:51:06 -0400

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -1,19 +1,12 @@
-// pos is the PacMan image position variable- it is set to 0 initially
-var pos = 0;
-//pageWidth is the width of the webpage. This is later used to calculate when Pac-Man needs to turn around. 
+var pos = 0; 
 let pageWidth = window.innerWidth;
-//This array contains all the PacMan movement images
 const pacArray = [
   ['PacMan1.png', 'PacMan2.png'],
   ['PacMan3.png', 'PacMan4.png'],
 ];
 
-// this variable defines what direction should PacMan go into:
-// 0 = left to right
-// 1 = right to left (reverse)
 var direction = 0;
 
-// This variable helps determine which PacMan image should be displayed. It flips between values 0 and 1
 var focus = 0;
 
 // This function is called on mouse click. Every time it is called, it updates the PacMan image, position and direction on the screen.
